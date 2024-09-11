a51eafc09472db9da76a8d4d6b0d83f6ac4815e9
seankudryk <seankudryk@hotmail.ca>
2024-09-11 17:21:01 -0400

    end of day update

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const addBookButton = document.querySelector("#add-book-button");
+const nameInput = document.querySelector("#name-input");
+const titleInput = document.querySelector("#title-input");
+const readStatusInput = document.querySelector("#read-status-input");
 
 const library = [];
+readStatusInput.value = "";
 
 function Book(author, title, readStatus) {
     this.author = author;
@@ -8,16 +12,21 @@ function Book(author, title, readStatus) {
     this.readStatus = readStatus;
 }
 
-let authorValue = 'Sean Kudryk';
-let titleValue = 'How to Catch a Big Billy Bass';
-let readStatus = true;
-
 function addBookToLibrary() {
-    const newBook = new Book(authorValue, titleValue, readStatus);
+    const newBook = new Book(nameInput.value, titleInput.value, readStatusInput.value);
     library.push(newBook);
 }
 
+function resetValues() {
+    nameInput.value = "";
+    titleInput.value = "";
+    readStatusInput.value = "";
+}
+
 addBookButton.addEventListener("click", () => {
+    //if any of the fields are empty, do not push values to library and require all 3 to be valid before proceeding
     addBookToLibrary();
+    resetValues();
     console.log(library);
 });
+
