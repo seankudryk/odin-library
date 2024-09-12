1b9453f8dbdc72da2417ba961e9103410ff9b0ea
seankudryk <seankudryk@hotmail.ca>
2024-09-12 13:05:33 -0400

    created a for loop to dynamically assign id's to each card. This will be recalled later on to identify the correct index in the library to delete once the  delete button is clicked

--- script.js
+++ script.js
@@ -7,14 +7,15 @@ const cardContainer = document.querySelector(".card-container");
 const library = [];
 readStatusInput.value = "";
 
-function Book(author, title, readStatus) {
+function Book(author, title, readStatus, index) {
     this.author = author;
     this.title = title;
     this.readStatus = readStatus;
+    this.index = index;
 }
 
 function addBookToLibrary() {
-    const newBook = new Book(nameInput.value, titleInput.value, readStatusInput.value);
+    const newBook = new Book(nameInput.value, titleInput.value, readStatusInput.value, library.length);
     library.push(newBook);
 }
 
@@ -28,12 +29,35 @@ addBookButton.addEventListener("click", () => {
     //if any of the fields are empty, do not push values to library and require all 3 to be valid before proceeding
     addBookToLibrary();
     resetValues();
-    createEntry();
+    createCard();
     console.log(library);
+    
 });
 
-function createEntry() {
+function createCard() {
+    //create the main div for the card and append it to the card container div
     const cardDiv = document.createElement("div");
     cardDiv.classList.add("card");
     cardContainer.appendChild(cardDiv);
+    //add content to the card
+
+    //set i to be equal to the library length - 1 (arrays are 0 indexed so i will always be equal to the most recently added index of the library array)
+    for (let i = library.length - 1; i === library.length - 1; i++) {
+        cardDiv.setAttribute("id",`${i}`);
+        cardDiv.innerHTML = 
+                `<div class="author-name-div card-row">
+                    <div class="author-name-label">Author Name: </div>
+                    <div class="author-name-output">${library[i].author}</div>
+                </div>
+                <div class="book-name-div card-row">
+                    <div class="book-name-label">Book Name: </div>
+                    <div class="book-name-output">${library[i].title}</div>
+                </div>
+                <div class="read-status-div card-row">
+                    <div class="read-status-label">Read Status: </div>
+                    <div class="read-status-output">${library[i].readStatus}</div>
+                </div>
+                <button class="delete-button">Delete Entry</button>
+                <button class="toggle-read">Toggle Read</button>`
+    }
 }
