a391f5a72bb01d82834b2c6f03687955d6aa621a
seankudryk <seankudryk@hotmail.ca>
2024-09-12 11:46:34 -0400

    succesfully adding new cards to the card container on addBookButton click

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const addBookButton = document.querySelector("#add-book-button");
 const nameInput = document.querySelector("#name-input");
 const titleInput = document.querySelector("#title-input");
 const readStatusInput = document.querySelector("#read-status-input");
+const cardContainer = document.querySelector(".card-container");
 
 const library = [];
 readStatusInput.value = "";
@@ -27,6 +28,12 @@ addBookButton.addEventListener("click", () => {
     //if any of the fields are empty, do not push values to library and require all 3 to be valid before proceeding
     addBookToLibrary();
     resetValues();
+    createEntry();
     console.log(library);
 });
 
+function createEntry() {
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+    cardContainer.appendChild(cardDiv);
+}
